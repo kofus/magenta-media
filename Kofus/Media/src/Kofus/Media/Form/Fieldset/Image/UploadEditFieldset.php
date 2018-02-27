767220b19b7e06d749e0aaa4afed20d70def3390
Ingo Bernhardt <bernhardt@kofus.de>
2018-02-27 19:06:21 +0100

    fix upload file

--- Kofus/Media/src/Kofus/Media/Form/Fieldset/Image/UploadEditFieldset.php
+++ Kofus/Media/src/Kofus/Media/Form/Fieldset/Image/UploadEditFieldset.php
@@ -1,29 +1,33 @@
 <?php
 namespace Kofus\Media\Form\Fieldset\Image;
 
-
 use Zend\Validator;
-
 
 class UploadEditFieldset extends UploadFieldset
 {
+
     public function getInputFilterSpecification()
     {
-        $spec = array(
-            'file' => array(
-            		'required' => false,
-            		 'validators' => array(
-            		 		array('name' => 'Zend\Validator\File\IsImage'),
-            		        array('name' => 'Zend\Validator\File\Size', 'options' => array(
-            		            'max' => $this->getMaxFilesize() . 'MB'
-            		        ))
-            		 )
-            ),
-        );
-
         // Hack: allow empty file in this case
-        //if (isset($_FILES['upload']['error']['file']) && 4 == $_FILES['upload']['error']['file'])
-        	//return $spec;
-        return $spec;
+        if (isset($_FILES['upload']['error']['file']) && 4 == $_FILES['upload']['error']['file'])
+            return array();
+            
+        return array(
+            'file' => array(
+                'required' => false,
+                'validators' => array(
+                    array(
+                        'name' => 'Zend\Validator\File\IsImage'
+                    ),
+                    array(
+                        'name' => 'Zend\Validator\File\Size',
+                        'options' => array(
+                            'max' => $this->getMaxFilesize() . 'MB'
+                        )
+                    )
+                )
+            )
+        );
+        
     }
 }
